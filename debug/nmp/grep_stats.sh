d673ea94ce5c47191ed6c0e9760ad39cac9e9751
Mario Paulo Drumond <mario.drumond@epfl.ch>
2017-04-26 16:49:20 +0200

    Experiments can run

--- debug/nmp/grep_stats.sh
+++ debug/nmp/grep_stats.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-stats="Nodes-uarch-Commits:NonSpin:User Nodes-uarch-InsnCount:User:Load Nodes-uarch-InsnCount:User:Store  Nodes-L1d-EvictsWithData Nodes-L1d-Fills Nodes-uarch-CoalescedStores"
+stats="Nodes-uarch-Commits:NonSpin:User Nodes-uarch-InsnCount:User:Load Nodes-uarch-InsnCount:User:Store  Nodes-L1d-EvictsWithData Nodes-L1d-Fills Nodes-uarch-CoalescedStores Nodes-batch-unroller0-dest-addressed-access Nodes-batch-unroller0-normal Nodes-fag-branches:cond"
 
 for stat in $stats; do
     grep $stat stats.raw
